0b5ba1c8eedc6dd203c03837e0ebf10739d4bd29
thoorpukarnakar <thoorpukarnakar@gmail.com>
2024-05-21 09:07:26 +0400

    updated examples

--- run_on_slurm.sh
+++ run_on_slurm.sh
@@ -8,11 +8,5 @@
 #SBATCH --gres=gpu:1 
 #SBATCH --time=1:00:00
 
-source /lustre1/tier2/users/karnakar.reddy/miniconda3/bin/activate
-conda activate mix_depths
-# Load required modules module load module_name
-pip install flash-attn
-pip install  -e /lustre1/tier2/users/karnakar.reddy/projects_2024/AutoGPTQ
-# Your job commands here python <your-script>
-python examples/quantization/basic_usage_gpt_xl.py
+
  
